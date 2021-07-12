fa31f31e9ef8e1491dc78b87ef8b37e86decf38f
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-13 01:37:59 +0200

    update: package.json * vite.config.ts specifies build dir

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build'
+  },
   plugins: [
     vue(),
     vueJsx({
