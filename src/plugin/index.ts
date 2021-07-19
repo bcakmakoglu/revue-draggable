cfcdbde27364a00e713197612cc94ccf7cd47974
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-20 01:56:16 +0200

    update: add components to Plugin

--- src/plugin/index.ts
+++ src/plugin/index.ts
@@ -1,7 +1,11 @@
 import { Plugin } from 'vue-demi';
 import DraggableDirective from '../directive/DraggableDirective';
+import Draggable from '../components/Draggable';
+import DraggableCore from '../components/DraggableCore';
 
 const plugin: Plugin = (app) => {
+  app.component('Draggable', Draggable);
+  app.component('DraggableCore', DraggableCore);
   app.directive('draggable', DraggableDirective);
 };
 
