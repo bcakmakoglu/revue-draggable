fa818749d100b30e9d39a43f18769041d302b979
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-20 11:52:50 +0200

    update: named instead of default export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export { default as useDraggableCore } from './hooks/useDraggableCore';
 export { default as useDraggable } from './hooks/useDraggable';
 export { default as DraggableDirective } from './directive/DraggableDirective';
 export { default as DraggablePlugin } from './plugin';
-export { default as default } from './components/Draggable';
+export { default as Draggable } from './components/Draggable';
 export { default as DraggableCore } from './components/DraggableCore';
