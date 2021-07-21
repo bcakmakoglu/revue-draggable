5e10ed9b108eddc43e50bc88ff17a77bc8ea2356
Braks <78412429+bcakmakoglu@users.noreply.github.com>
2021-07-22 00:57:41 +0200

    update: move colors to separate file * add some colors * windi confs

---
+++ example/colors.ts
@@ -0,0 +1,51 @@
+import windiColors from 'windicss/colors';
+
+export const colors = {
+  primary: {
+    '50': '#fcf9ff',
+    '100': '#f8f3ff',
+    '200': '#eee1fe',
+    '300': '#e4cffe',
+    '400': '#cfaafd',
+    '500': '#BB86FC',
+    '600': '#a879e3',
+    '700': '#8c65bd',
+    '800': '#705097',
+    '900': '#5c427b',
+    DEFAULT: '#BB86FC'
+  },
+  secondary: {
+    '50': '#c4fef9',
+    '100': '#9df7ef',
+    '200': '#77f0e4',
+    '300': '#50e8da',
+    '400': '#2ae1cf',
+    '500': '#03dac5',
+    '600': '#03ae9d',
+    '700': '#028276',
+    '800': '#02564e',
+    '900': '#012a26',
+    DEFAULT: '#03dac5'
+  },
+  accent: {
+    '50': '#e6d4ff',
+    '100': '#ccaafc',
+    '200': '#b17ff8',
+    '300': '#9755f5',
+    '400': '#7c2af1',
+    '500': '#6200ee',
+    '600': '#5000c1',
+    '700': '#3d0095',
+    '800': '#2b0068',
+    '900': '#18003b',
+    DEFAULT: '#6200ee'
+  },
+  light: windiColors.light,
+  gray: windiColors.coolGray,
+  blue: windiColors.lightBlue,
+  red: windiColors.rose,
+  pink: windiColors.fuchsia,
+  indigo: windiColors.indigo,
+  orange: windiColors.orange,
+  amber: windiColors.amber
+} as const;
--- vue2-demo/windicss.config.ts
+++ vue2-demo/windicss.config.ts
@@ -1,54 +1,6 @@
 import { defineConfig } from 'windicss/helpers';
 import typography from 'windicss/plugin/typography';
-import windiColors from 'windicss/colors';
-
-export const colors = {
-  'primary-gradient': 'bg-gradient-to-r from-purple-200 via-purple-400 to-purple-800',
-  primary: {
-    '50': '#fcf9ff',
-    '100': '#f8f3ff',
-    '200': '#eee1fe',
-    '300': '#e4cffe',
-    '400': '#cfaafd',
-    '500': '#BB86FC',
-    '600': '#a879e3',
-    '700': '#8c65bd',
-    '800': '#705097',
-    '900': '#5c427b',
-    DEFAULT: '#BB86FC'
-  },
-  secondary: {
-    '50': '#c4fef9',
-    '100': '#9df7ef',
-    '200': '#77f0e4',
-    '300': '#50e8da',
-    '400': '#2ae1cf',
-    '500': '#03dac5',
-    '600': '#03ae9d',
-    '700': '#028276',
-    '800': '#02564e',
-    '900': '#012a26',
-    DEFAULT: '#03dac5'
-  },
-  accent: {
-    '50': '#e6d4ff',
-    '100': '#ccaafc',
-    '200': '#b17ff8',
-    '300': '#9755f5',
-    '400': '#7c2af1',
-    '500': '#6200ee',
-    '600': '#5000c1',
-    '700': '#3d0095',
-    '800': '#2b0068',
-    '900': '#18003b',
-    DEFAULT: '#6200ee'
-  },
-  light: '#F2F2F2',
-  gray: windiColors.coolGray,
-  blue: windiColors.lightBlue,
-  red: windiColors.rose,
-  pink: windiColors.fuchsia
-} as const;
+import { colors } from '../example/colors';
 
 export default defineConfig({
   theme: {
